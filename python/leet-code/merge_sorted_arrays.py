0061bc25a5fc4201e247cd5f342c13591593cc1e
MatheusXimeness <matheus.ximenes2016@outlook.com>
2026-09-10 14:56:25 -0300

    leet-code-easy: merge sorted arrays

---
+++ python/leet-code/merge_sorted_arrays.py
@@ -0,0 +1,33 @@
+def merge_sorted_arrays(nums1, m, nums2, n):
+    i = m - 1
+    j = n - 1
+    k = m + n - 1
+
+    while j >= 0:
+        if i >= 0 and nums1[i] > nums2[j]:
+            nums1[k] = nums1[i]
+            i -= 1
+        else:
+            nums1[k] = nums2[j]
+            j -= 1
+        k -= 1
+
+
+nums1 = [1,2,3,0,0,0]
+m = 3
+nums2 = [2,5,6]
+n = 3
+
+# nums1 = [1]
+# m = 1
+# nums2 = []
+# n = 0
+
+# nums1 = [0]
+# m = 0
+# nums2 = [1]
+# n = 1
+
+merge_sorted_arrays(nums1, m, nums2, n)
+
+print(nums1)
