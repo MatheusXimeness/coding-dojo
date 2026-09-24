e4868799cca658b204bbb429b0bce4f894f605d1
MatheusXimeness <matheus.ximenes2016@outlook.com>
2026-09-24 14:18:04 -0300

    leet code - majority element

---
+++ python/leet-code/majority_element.py
@@ -0,0 +1,33 @@
+from collections import Counter
+# [2,2,1,1,1,2,2]
+# [2,2,1,1,1,1,1,2,2]
+
+def majority_element_with_more_space(nums):
+    majority_limit = len(nums) // 2
+    counts = Counter(nums)
+    result = 0
+
+    for key, value in counts.items():
+        if value > majority_limit:
+            majority_limit = value
+            result = key
+    return result
+
+def majority_element_better_solution(nums):
+    count = 0
+    candidate = 0 
+    for num in nums:
+        if num != candidate:
+            if count == 0:
+                candidate = num
+                count = 1
+            else:
+                count -= 1
+        else:
+            count += 1
+
+    return candidate
+
+
+nums = [2,2,1,1,1,1,1,2,2]
+print(majority_element_better_solution(nums))
